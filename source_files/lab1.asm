58da833ce538610560b8f74eb8eeb2a42d517910
the-nostromo-1 <dtinley@kent.edu>
2023-09-22 20:26:26 -0400

    Lab 1 progress

--- source_files/lab1.asm
+++ source_files/lab1.asm
@@ -5,27 +5,42 @@
 # Write a MIPS program to compute f = g - (f + 5)
 #
 
-# Macros
-.macro print_line()
-	li $v0, 4
-	la $a0, _new_line
+# MACROS #########################################################
+.macro print_string(%string) # macro for printing string parameter
+	li $v0, 4                # load syscall for print string in $v0
+	la $a0, %string          # load address of string to be printed
 	syscall
 .end_macro
 
-# Data
+.macro input(%num)          # macro for inputing integer
+	li $v0, 5               # load syscall for reading integer
+	la $a0, %num            # load address of integer to be input
+	syscall                 # value stored in $v0
+.end_macro
+##################################################################
+
+# DATA ###########################################################
 .data
-	_f: .word 0
-	_g: .word 0
+	_f: .word 0           # 32 bit integer for f
+	_g: .word 0           # 32 bit integer for g
 	
 	_new_line: .asciiz "\n"
+	_f_prompt: .asciiz "Enter a value for f: "
+	_g_prompt: .asciiz "Enter a value for g: "
+##################################################################
+
+# ALIGNMENT ######################################################
+.align 2 # align memory to 2^2, so 4 for word
+##################################################################
+
+# TEXT ###########################################################
+.text
+
+	print_string(_f_prompt)
+	
+	input(_f)
 
 
 
 
-
-
-
-
-
-
-
+##################################################################
